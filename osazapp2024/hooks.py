bf182ed2844b453bf4ee3023622631fa9cadf2b0
sp9leo <sp9leonardo@gmail.com>
2024-09-24 07:59:12 +0000

    add roles

--- osazapp2024/hooks.py
+++ osazapp2024/hooks.py
@@ -9,7 +9,14 @@ app_license = "mit"
 # Includes in <head>
 # ------------------
 fixtures = [
-    {"dt": "Workspace", "filters": [["name", "in", ["Prehrana_workspace-Administrator"]]]}
+    {"dt": "Workspace", "filters": [["name", "in", ["Prehrana_workspace-Administrator"]]]},
+     "Role Profile",
+    {
+        "doctype": "Role",
+        "filters": [
+            ["name", "in", ["Starsi", "Prehrana"]]
+        ]
+    }
 ]
 
 # include js, css files in header of desk.html
